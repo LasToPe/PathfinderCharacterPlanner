78d264f64e2a09fe4672758f010a16ee435a0274
Lasse Toftegaard Pedersen <laped@orifarm.com>
2021-03-21 15:45:35 +0100

    Paladin

---
+++ PathfinderModelling/Model/Classes/Paladin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfinderModelling.Model.Classes
+{
+    public class Paladin : CharacterClass
+    {
+        public Paladin()
+        {
+            Name = "Paldin";
+            Alignment = "Lawful Good";
+            BaseAttackBonus = BaseAttackBonus.Full;
+            FortitudeSave = SavingThrow.Good;
+            ReflexSave = SavingThrow.Bad;
+            WillSave = SavingThrow.Good;
+            ClassAbilities = new List<ClassAbility>
+            {
+                new ClassAbility { Name = "Aura of Good", Level = 1 },
+                new ClassAbility { Name = "Detect Evil", Level = 1 },
+                new ClassAbility { Name = "Smite Evil 1/day", Level = 1 },
+                new ClassAbility { Name = "Smite Evil 2/day", Level = 4 },
+                new ClassAbility { Name = "Smite Evil 3/day", Level = 7 },
+                new ClassAbility { Name = "Smite Evil 4/day", Level = 10 },
+                new ClassAbility { Name = "Smite Evil 5/day", Level = 13 },
+                new ClassAbility { Name = "Smite Evil 6/day", Level = 16 },
+                new ClassAbility { Name = "Smite Evil 7/day", Level = 19 },
+                new ClassAbility { Name = "Lay on Hands", Level = 2 },
+                new ClassAbility { Name = "Divine Grace", Level = 2 },
+                new ClassAbility { Name = "Aura of Courage", Level = 3 },
+                new ClassAbility { Name = "Divine Health", Level = 3 },
+                new ClassAbility { Name = "Mercy", Level = 3 },
+                new ClassAbility { Name = "Mercy", Level = 6 },
+                new ClassAbility { Name = "Mercy", Level = 9 },
+                new ClassAbility { Name = "Mercy", Level = 12 },
+                new ClassAbility { Name = "Mercy", Level = 15 },
+                new ClassAbility { Name = "Mercy", Level = 18 },
+                new ClassAbility { Name = "Channel Positive Energy", Level = 4 },
+                new ClassAbility { Name = "Spells", Level = 4 },
+                new ClassAbility { Name = "Divine Bond", Level = 5 },
+                new ClassAbility { Name = "Aura of Resolve", Level = 8 },
+                new ClassAbility { Name = "Aura of Justice", Level = 11 },
+                new ClassAbility { Name = "Aura of Faith", Level = 14 },
+                new ClassAbility { Name = "Aura of Righteousness", Level = 17 },
+                new ClassAbility { Name = "Holy Champion", Level = 20 },
+            };
+        }
+    }
+}
