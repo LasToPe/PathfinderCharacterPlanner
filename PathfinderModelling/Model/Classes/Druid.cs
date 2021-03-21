ba384646c3c75454c096aaa5d4de1880e4382515
Lasse Toftegaard Pedersen <laped@orifarm.com>
2021-03-21 12:25:36 +0100

    Added druid

---
+++ PathfinderModelling/Model/Classes/Druid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfinderModelling.Model.Classes
+{
+    public class Druid : CharacterClass
+    {
+        public Druid()
+        {
+            Name = "Druid";
+            Alignment = "Any neutral";
+            BaseAttackBonus = BaseAttackBonus.Medium;
+            FortitudeSave = SavingThrow.Good;
+            ReflexSave = SavingThrow.Bad;
+            WillSave = SavingThrow.Good;
+            ClassAbilities = new List<ClassAbility>
+            {
+                new ClassAbility { Name = "Nature Bond", Level = 1 },
+                new ClassAbility { Name = "Nature Sense", Level = 1 },
+                new ClassAbility { Name = "Orisons", Level = 1 },
+                new ClassAbility { Name = "Spells", Level = 1 },
+                new ClassAbility { Name = "Spontaneous Casting", Level = 1 },
+                new ClassAbility { Name = "Wild Empathy", Level = 1 },
+                new ClassAbility { Name = "Woodland Stride", Level = 2 },
+                new ClassAbility { Name = "Trackless Step", Level = 3 },
+                new ClassAbility { Name = "Resist Nature's Lure", Level = 4 },
+                new ClassAbility { Name = "Wild Shape 1/day", Level = 4 },
+                new ClassAbility { Name = "Wild Shape 2/day", Level = 6 },
+                new ClassAbility { Name = "Wild Shape 3/day", Level = 8 },
+                new ClassAbility { Name = "Wild Shape 4/day", Level = 10 },
+                new ClassAbility { Name = "Wild Shape 5/day", Level = 12 },
+                new ClassAbility { Name = "Wild Shape 6/day", Level = 14 },
+                new ClassAbility { Name = "Wild Shape 7/day", Level = 16 },
+                new ClassAbility { Name = "Wild Shape 8/day", Level = 18 },
+                new ClassAbility { Name = "Wild Shape at Will", Level = 20 },
+                new ClassAbility { Name = "Venom Immunity", Level = 9 },
+                new ClassAbility { Name = "A Thousand Faces", Level = 13 },
+                new ClassAbility { Name = "Timeless Body", Level = 15 },
+            };
+        }
+    }
+}
