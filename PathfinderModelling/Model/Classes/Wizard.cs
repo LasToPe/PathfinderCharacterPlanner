d7cee12d9d65fd42961927bb833c4a7e798a40e4
Lasse Toftegaard Pedersen <laped@orifarm.com>
2021-03-21 18:23:42 +0100

    Wizard

---
+++ PathfinderModelling/Model/Classes/Wizard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfinderModelling.Model.Classes
+{
+    class Wizard : CharacterClass
+    {
+        public Wizard()
+        {
+            Name = "Wizard";
+            BaseAttackBonus = BaseAttackBonus.Half;
+            FortitudeSave = SavingThrow.Bad;
+            ReflexSave = SavingThrow.Bad;
+            WillSave = SavingThrow.Good;
+            ClassAbilities = new List<ClassAbility>
+            {
+                new ClassAbility { Name = "Arcane Bond", Level = 1 },
+                new ClassAbility { Name = "Arcane School", Level = 1 },
+                new ClassAbility { Name = "Cantrips", Level = 1 },
+                new ClassAbility { Name = "Scribe Scroll", Level = 1 },
+                new ClassAbility { Name = "Spells", Level = 1 },
+                new ClassAbility { Name = "Bonus Feat", Level = 5 },
+                new ClassAbility { Name = "Bonus Feat", Level = 10 },
+                new ClassAbility { Name = "Bonus Feat", Level = 15 },
+                new ClassAbility { Name = "Bonus Feat", Level = 20 },
+            };
+        }
+    }
+}
