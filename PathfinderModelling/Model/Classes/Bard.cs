52bc016485dcee839cf759778f9974a43e2a40ff
Lasse Toftegaard Pedersen <laped@orifarm.com>
2021-03-23 18:40:45 +0100

    Addded dawnflower dervish to bard and virtuous bravo to paladin

--- PathfinderModelling/Model/Classes/Bard.cs
+++ PathfinderModelling/Model/Classes/Bard.cs
@@ -41,42 +41,120 @@ namespace PathfinderModelling.Model.Classes
                     new CasterLevel(20, null, 5, 5, 5, 5, 5, 5),
                 }
             };
+
             ClassAbilities = new List<ClassAbility>
             {
-                new ClassAbility { Name = "Bardic Knowledge", Level = 1 },
-                new ClassAbility { Name = "Bardic Performance", Level = 1 },
-                new ClassAbility { Name = "Cantrips", Level = 1 },
-                new ClassAbility { Name = "Countersong", Level = 1 },
-                new ClassAbility { Name = "Distraction", Level = 1 },
-                new ClassAbility { Name = "Fascinate", Level = 1 },
-                new ClassAbility { Name = "Inspire Courage +1", Level = 1 },
-                new ClassAbility { Name = "Inspire Courage +2", Level = 5 },
-                new ClassAbility { Name = "Inspire Courage +3", Level = 11 },
-                new ClassAbility { Name = "Inspire Courage +4", Level = 17 },
-                new ClassAbility { Name = "Spells", Level = 1 },
-                new ClassAbility { Name = "Inspire Competence +2", Level = 3 },
-                new ClassAbility { Name = "Inspire Competence +3", Level = 7 },
-                new ClassAbility { Name = "Inspire Competence +4", Level = 11 },
-                new ClassAbility { Name = "Inspire Competence +5", Level = 15 },
-                new ClassAbility { Name = "Inspire Competence +6", Level = 19 },
-                new ClassAbility { Name = "Suggestion", Level = 6 },
-                new ClassAbility { Name = "Dirge of Doom", Level = 8 },
-                new ClassAbility { Name = "Inspire Greatness", Level = 9 },
-                new ClassAbility { Name = "Soothing Performance", Level = 12 },
-                new ClassAbility { Name = "Frightening Tune", Level = 14 },
-                new ClassAbility { Name = "Inspire Heroics", Level = 15 },
-                new ClassAbility { Name = "Mass Suggestion", Level = 18 },
-                new ClassAbility { Name = "Deadly Performance", Level = 20 },
-                new ClassAbility { Name = "Veratile Performance", Level = 2 },
-                new ClassAbility { Name = "Veratile Performance", Level = 6 },
-                new ClassAbility { Name = "Veratile Performance", Level = 10 },
-                new ClassAbility { Name = "Veratile Performance", Level = 14 },
-                new ClassAbility { Name = "Veratile Performance", Level = 18 },
-                new ClassAbility { Name = "Lore Master 1/day", Level = 5 },
-                new ClassAbility { Name = "Lore Master 2/day", Level = 11 },
-                new ClassAbility { Name = "Lore Master 3/day", Level = 17 },
-                new ClassAbility { Name = "Jack-of-All-Trades", Level = 10 },
+                _bardicKnowledge,
+                _bardicPerformance,
+                _cantrips,
+                _countersong,
+                _distraction,
+                _fascinate,
+                _inspireCourage1,
+                _inspireCourage2,
+                _inspireCourage3,
+                _inspireCourage4,
+                _spells,
+                _inspireCompetence2,
+                _suggestion,
+                _dirgeOfDoom,
+                _inspireGreatness,
+                _soothingPerformance,
+                _frighteningTune,
+                _inspireHeroics,
+                _massSuggestion,
+                _deadlyPerformance,
+                _versatilePerformance2,
+                _versatilePerformance6,
+                _versatilePerformance10,
+                _versatilePerformance14,
+                _versatilePerformance18,
+                _loreMaster1,
+                _loreMaster2,
+                _loreMaster3,
+                _jackOfAllTrades
             };
+
+            Archetypes.Add(DawnflowerDervish);
+        }
+
+        #region Class Abilities
+        ClassAbility _bardicKnowledge = new ClassAbility { Name = "Bardic Knowledge", Level = 1 };
+        ClassAbility _bardicPerformance = new ClassAbility { Name = "Bardic Performance", Level = 1 };
+        ClassAbility _cantrips = new ClassAbility { Name = "Cantrips", Level = 1 };
+        ClassAbility _countersong = new ClassAbility { Name = "Countersong", Level = 1 };
+        ClassAbility _distraction = new ClassAbility { Name = "Distraction", Level = 1 };
+        ClassAbility _fascinate = new ClassAbility { Name = "Fascinate", Level = 1 };
+        ClassAbility _inspireCourage1 = new ClassAbility { Name = "Inspire Courage +1", Level = 1 };
+        ClassAbility _inspireCourage2 = new ClassAbility { Name = "Inspire Courage +2", Level = 5 };
+        ClassAbility _inspireCourage3 = new ClassAbility { Name = "Inspire Courage +3", Level = 11 };
+        ClassAbility _inspireCourage4 = new ClassAbility { Name = "Inspire Courage +4", Level = 17 };
+        ClassAbility _spells = new ClassAbility { Name = "Spells", Level = 1 };
+        ClassAbility _inspireCompetence2 = new ClassAbility { Name = "Inspire Competence +2", Level = 3 };
+        ClassAbility _inspireCompetence3 = new ClassAbility { Name = "Inspire Competence +3", Level = 7 };
+        ClassAbility _inspireCompetence4 = new ClassAbility { Name = "Inspire Competence +4", Level = 11 };
+        ClassAbility _inspireCompetence5 = new ClassAbility { Name = "Inspire Competence +5", Level = 15 };
+        ClassAbility _inspireCompetence6 = new ClassAbility { Name = "Inspire Competence +6", Level = 19 };
+        ClassAbility _suggestion = new ClassAbility { Name = "Suggestion", Level = 6 };
+        ClassAbility _dirgeOfDoom = new ClassAbility { Name = "Dirge of Doom", Level = 8 };
+        ClassAbility _inspireGreatness = new ClassAbility { Name = "Inspire Greatness", Level = 9 };
+        ClassAbility _soothingPerformance = new ClassAbility { Name = "Soothing Performance", Level = 12 };
+        ClassAbility _frighteningTune = new ClassAbility { Name = "Frightening Tune", Level = 14 };
+        ClassAbility _inspireHeroics = new ClassAbility { Name = "Inspire Heroics", Level = 15 };
+        ClassAbility _massSuggestion = new ClassAbility { Name = "Mass Suggestion", Level = 18 };
+        ClassAbility _deadlyPerformance = new ClassAbility { Name = "Deadly Performance", Level = 20 };
+        ClassAbility _versatilePerformance2 = new ClassAbility { Name = "Veratile Performance", Level = 2 };
+        ClassAbility _versatilePerformance6 = new ClassAbility { Name = "Veratile Performance", Level = 6 };
+        ClassAbility _versatilePerformance10 = new ClassAbility { Name = "Veratile Performance", Level = 10 };
+        ClassAbility _versatilePerformance14 = new ClassAbility { Name = "Veratile Performance", Level = 14 };
+        ClassAbility _versatilePerformance18 = new ClassAbility { Name = "Veratile Performance", Level = 18 };
+        ClassAbility _loreMaster1 = new ClassAbility { Name = "Lore Master 1/day", Level = 5 };
+        ClassAbility _loreMaster2 = new ClassAbility { Name = "Lore Master 2/day", Level = 11 };
+        ClassAbility _loreMaster3 = new ClassAbility { Name = "Lore Master 3/day", Level = 17 };
+        ClassAbility _jackOfAllTrades = new ClassAbility { Name = "Jack-of-All-Trades", Level = 10 };
+        #endregion
+
+        Archetype DawnflowerDervish
+        {
+            get
+            {
+                return new Archetype
+                {
+                    Name = "Dawnflower Dervish",
+                    ArchetypeAbilities = new List<ArchetypeAbility>
+                    {
+                        new ArchetypeAbility
+                        {
+                            Name = "Deity",
+                            Level = 1,
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Battle Dance",
+                            Level = 1,
+                            ReplacedAbilities = new List<ClassAbility> { _bardicPerformance }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Dervish Dance",
+                            Level = 1,
+                            ReplacedAbilities = new List<ClassAbility> { _bardicKnowledge }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Spinning Spellcaster",
+                            Level = 5,
+                            ReplacedAbilities = new List<ClassAbility> { _loreMaster1, _loreMaster2, _loreMaster3 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Meditative Whirl",
+                            Level = 8,
+                            ReplacedAbilities = new List<ClassAbility> { _dirgeOfDoom }
+                        },
+                    }
+                };
+            }
         }
     }
 }
--- PathfinderModelling/Model/Classes/Paladin.cs
+++ PathfinderModelling/Model/Classes/Paladin.cs
@@ -43,34 +43,173 @@ namespace PathfinderModelling.Model.Classes
             };
             ClassAbilities = new List<ClassAbility>
             {
-                new ClassAbility { Name = "Aura of Good", Level = 1 },
-                new ClassAbility { Name = "Detect Evil", Level = 1 },
-                new ClassAbility { Name = "Smite Evil 1/day", Level = 1 },
-                new ClassAbility { Name = "Smite Evil 2/day", Level = 4 },
-                new ClassAbility { Name = "Smite Evil 3/day", Level = 7 },
-                new ClassAbility { Name = "Smite Evil 4/day", Level = 10 },
-                new ClassAbility { Name = "Smite Evil 5/day", Level = 13 },
-                new ClassAbility { Name = "Smite Evil 6/day", Level = 16 },
-                new ClassAbility { Name = "Smite Evil 7/day", Level = 19 },
-                new ClassAbility { Name = "Lay on Hands", Level = 2 },
-                new ClassAbility { Name = "Divine Grace", Level = 2 },
-                new ClassAbility { Name = "Aura of Courage", Level = 3 },
-                new ClassAbility { Name = "Divine Health", Level = 3 },
-                new ClassAbility { Name = "Mercy", Level = 3 },
-                new ClassAbility { Name = "Mercy", Level = 6 },
-                new ClassAbility { Name = "Mercy", Level = 9 },
-                new ClassAbility { Name = "Mercy", Level = 12 },
-                new ClassAbility { Name = "Mercy", Level = 15 },
-                new ClassAbility { Name = "Mercy", Level = 18 },
-                new ClassAbility { Name = "Channel Positive Energy", Level = 4 },
-                new ClassAbility { Name = "Spells", Level = 4 },
-                new ClassAbility { Name = "Divine Bond", Level = 5 },
-                new ClassAbility { Name = "Aura of Resolve", Level = 8 },
-                new ClassAbility { Name = "Aura of Justice", Level = 11 },
-                new ClassAbility { Name = "Aura of Faith", Level = 14 },
-                new ClassAbility { Name = "Aura of Righteousness", Level = 17 },
-                new ClassAbility { Name = "Holy Champion", Level = 20 },
+                _auraOfGood,
+                _detectEvil,
+                _smiteEvil1,
+                _smiteEvil2,
+                _smiteEvil3,
+                _smiteEvil4,
+                _smiteEvil5,
+                _smiteEvil6,
+                _smiteEvil7,
+                _layOnHands,
+                _divineGrace,
+                _auraOfCourage,
+                _divineHealth,
+                _mercy3,
+                _mercy6,
+                _mercy9,
+                _mercy12,
+                _mercy15,
+                _mercy18,
+                _channelPositiveEnergy,
+                _spells,
+                _divineBond,
+                _auraOfResolve,
+                _auraOfJustice,
+                _auraOfFaith,
+                _auraOfRighteousness,
+                _holyChampion,
             };
+
+            Archetypes.Add(VirtuousBravo);
+        }
+
+        #region Class Abilities
+        ClassAbility _auraOfGood = new ClassAbility { Name = "Aura of Good", Level = 1 };
+        ClassAbility _detectEvil = new ClassAbility { Name = "Detect Evil", Level = 1 };
+        ClassAbility _smiteEvil1 = new ClassAbility { Name = "Smite Evil 1/day", Level = 1 };
+        ClassAbility _smiteEvil2 = new ClassAbility { Name = "Smite Evil 2/day", Level = 4 };
+        ClassAbility _smiteEvil3 = new ClassAbility { Name = "Smite Evil 3/day", Level = 7 };
+        ClassAbility _smiteEvil4 = new ClassAbility { Name = "Smite Evil 4/day", Level = 10 };
+        ClassAbility _smiteEvil5 = new ClassAbility { Name = "Smite Evil 5/day", Level = 13 };
+        ClassAbility _smiteEvil6 = new ClassAbility { Name = "Smite Evil 6/day", Level = 16 };
+        ClassAbility _smiteEvil7 = new ClassAbility { Name = "Smite Evil 7/day", Level = 19 };
+        ClassAbility _layOnHands = new ClassAbility { Name = "Lay on Hands", Level = 2 };
+        ClassAbility _divineGrace = new ClassAbility { Name = "Divine Grace", Level = 2 };
+        ClassAbility _auraOfCourage = new ClassAbility { Name = "Aura of Courage", Level = 3 };
+        ClassAbility _divineHealth = new ClassAbility { Name = "Divine Health", Level = 3 };
+        ClassAbility _mercy3 = new ClassAbility { Name = "Mercy", Level = 3 };
+        ClassAbility _mercy6 = new ClassAbility { Name = "Mercy", Level = 6 };
+        ClassAbility _mercy9 = new ClassAbility { Name = "Mercy", Level = 9 };
+        ClassAbility _mercy12 = new ClassAbility { Name = "Mercy", Level = 12 };
+        ClassAbility _mercy15 = new ClassAbility { Name = "Mercy", Level = 15 };
+        ClassAbility _mercy18 = new ClassAbility { Name = "Mercy", Level = 18 };
+        ClassAbility _channelPositiveEnergy = new ClassAbility { Name = "Channel Positive Energy", Level = 4 };
+        ClassAbility _spells = new ClassAbility { Name = "Spells", Level = 4 };
+        ClassAbility _divineBond = new ClassAbility { Name = "Divine Bond", Level = 5 };
+        ClassAbility _auraOfResolve = new ClassAbility { Name = "Aura of Resolve", Level = 8 };
+        ClassAbility _auraOfJustice = new ClassAbility { Name = "Aura of Justice", Level = 11 };
+        ClassAbility _auraOfFaith = new ClassAbility { Name = "Aura of Faith", Level = 14 };
+        ClassAbility _auraOfRighteousness = new ClassAbility { Name = "Aura of Righteousness", Level = 17 };
+        ClassAbility _holyChampion = new ClassAbility { Name = "Holy Champion", Level = 20 };
+        #endregion
+
+        Archetype VirtuousBravo
+        {
+            get
+            {
+                return new Archetype
+                {
+                    Name = "Virtuous Bravo",
+                    ArchetypeAbilities = new List<ArchetypeAbility>
+                    {
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Finesse",
+                            Level = 1
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Smite 1/day",
+                            Level = _smiteEvil1.Level,
+                            ReplacedAbilities = new List<ClassAbility> { _smiteEvil1 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Smite 2/day",
+                            Level = _smiteEvil2.Level,
+                            ReplacedAbilities = new List<ClassAbility> { _smiteEvil2 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Smite 3/day",
+                            Level = _smiteEvil3.Level,
+                            ReplacedAbilities = new List<ClassAbility> { _smiteEvil3 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Smite 4/day",
+                            Level = _smiteEvil4.Level,
+                            ReplacedAbilities = new List<ClassAbility> { _smiteEvil4 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Smite 5/day",
+                            Level = _smiteEvil5.Level,
+                            ReplacedAbilities = new List<ClassAbility> { _smiteEvil5 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Smite 6/day",
+                            Level = _smiteEvil6.Level,
+                            ReplacedAbilities = new List<ClassAbility> { _smiteEvil6 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Smite 7/day",
+                            Level = _smiteEvil7.Level,
+                            ReplacedAbilities = new List<ClassAbility> { _smiteEvil7 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Nimble +1",
+                            Level = 3,
+                            ReplacedAbilities = new List<ClassAbility> { _mercy3, _mercy6, _mercy9, _mercy12, _mercy15, _mercy18 }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Nimble +2",
+                            Level = 7,
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Nimble +3",
+                            Level = 11,
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Nimble +4",
+                            Level = 15,
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Nimble +5",
+                            Level = 19,
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Panache and Deeds",
+                            Level = 4,
+                            ReplacedAbilities = new List<ClassAbility> { _spells },
+                            Add = () => SpellCaster = false,
+                            Remove = () => SpellCaster = true,
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Advanced Deeds",
+                            Level = 11,
+                            ReplacedAbilities = new List<ClassAbility> { _auraOfJustice }
+                        },
+                        new ArchetypeAbility
+                        {
+                            Name = "Bravo's Holy Strike",
+                            Level = 20,
+                            ReplacedAbilities = new List<ClassAbility> { _holyChampion }
+                        }
+                    }
+                };
+            }
         }
     }
 }
