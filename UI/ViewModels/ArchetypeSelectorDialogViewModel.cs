93f32e52bd62eea3f27600728f612d761fb8a7ca
Lasse Toftegaard Pedersen <laped@orifarm.com>
2021-03-27 15:16:21 +0100

    Archetype selector only shows available archetypes

--- UI/ViewModels/ArchetypeSelectorDialogViewModel.cs
+++ UI/ViewModels/ArchetypeSelectorDialogViewModel.cs
@@ -2,6 +2,7 @@
 using PathfinderModelling.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace UI.ViewModels
@@ -9,6 +10,21 @@ namespace UI.ViewModels
     public class ArchetypeSelectorDialogViewModel : ViewModelBase
     {
         public CharacterClass Class { get; set; }
-        public List<Archetype> Archetypes => Class.Archetypes;
+        private List<Archetype> Archetypes => Class.Archetypes;
+        public List<Archetype> AvailableArcheTypes
+        {
+            get
+            {
+                var list = new List<Archetype>();
+                foreach (var a in Archetypes)
+                {
+                    if (a.ArchetypeAbilities.SelectMany(a => a.ReplacedAbilities).All(a => Class.ClassAbilities.Contains(a)))
+                    {
+                        list.Add(a);
+                    }
+                }
+                return list;
+            }
+        }
     }
 }
